3c422f67e4f2dc0a9339789e0f705be8379fe23d
Muhammad Aliyan <aliyantarar4@gmail.com>
2026-03-18 12:21:27 +0500

    Fix seeder to work without factories on production

--- api/database/seeders/DatabaseSeeder.php
+++ api/database/seeders/DatabaseSeeder.php
@@ -17,10 +17,14 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // Manually create admin user to avoid factory discovery issues on some servers
+        \App\Models\User::updateOrCreate(
+            ['email' => 'test@example.com'],
+            [
+                'name' => 'Admin User',
+                'password' => \Illuminate\Support\Facades\Hash::make('password'),
+            ]
+        );
 
         $this->call([
             TestDataSeeder::class,
